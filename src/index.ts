c4440a17149020d960b32d4059799118836d5a88
Juan <juan_jg@hotmail.com>
2024-06-26 17:48:48 -0300

    add Log http req & res middleware

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    console.log(`${req.method} ${req.originalUrl} ${res.statusCode}`);
+  });
+  next();
+});
+
 app.use("/users", usersRoutes);
 
 export { app };
